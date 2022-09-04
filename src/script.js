09f84c2b9947fe112c957f6285a7387982b3568d
Vitor César <65216891+lordvitor11@users.noreply.github.com>
2022-09-04 01:37:47 -0300

    Update
    *Imagens movidas para a pasta src;
    
    *Criação da página de cadastro;
    
    *Criação da segunda página de cadastro para verificação de senha;
    
    *Verificação de requisitos de senha no javascript.

--- src/script.js
+++ src/script.js
@@ -9,3 +9,42 @@ function login() {
     }
 }
 
+function textChange() {
+    var passwd = document.getElementById("passwd").value
+    var passwd1 = document.getElementById("passwd1");
+    var passwd2 = document.getElementById("passwd2");
+    var passwd3 = document.getElementById("passwd3");
+    var upper = false;
+    var lower = false;
+
+    for (var c = 0; c < passwd.length; c++) {
+        if (passwd[c] === passwd[c].toUpperCase() && passwd[c] != parseInt(passwd[c])) {
+            upper = true; break;
+        }
+    }
+
+    for (var c = 0; c < passwd.length; c++) {
+        if (passwd[c] === passwd[c].toLowerCase() && passwd[c] != parseInt(passwd[c])) {
+            lower = true; break
+        }
+    }
+
+    if (passwd.length >= 6) {
+        passwd1.style.color = "green";
+    } else {
+        passwd1.style.color = "red";
+    }
+
+    if (upper) {
+        passwd2.style.color = "green";
+    } else {
+        passwd2.style.color = "red";
+    }
+
+    if (lower) {
+        passwd3.style.color = "green";
+    } else {
+        passwd3.style.color = "red";
+    }
+}
+
